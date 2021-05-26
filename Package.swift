9ce1a6a06531b9703952888abc0aaffcd1bc7e79
Ahmad Khabbaz <khabbaz.kayisoft@gmail.com>
2021-05-26 14:23:29 +0300

    chore: Update the CocoaAsyncSocket Version to 7.6.5

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .library(name: "CocoaMQTTWebSocket", targets: [ "CocoaMQTTWebSocket" ])
     ],
     dependencies: [
-        .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket", from: "7.6.4"),
+        .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket", .exact("7.6.5") ),
         .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.0.2"),
     ],
     targets: [
